18640180a147d7fd68ffb1517d266fd73c193aad
MuhamedAbdalla <medokingdom7@outlook.com>
2020-10-30 04:14:59 +0200

    loginscreen

--- src/screen/splash_screen/splash_screen.js
+++ src/screen/splash_screen/splash_screen.js
@@ -4,15 +4,15 @@ import "./style.css";
 class Splash_Screen extends React.Component {
   render() {
     return (
-      <div class="loading">
-        <div class="obj"></div>
-        <div class="obj"></div>
-        <div class="obj"></div>
-        <div class="obj"></div>
-        <div class="obj"></div>
-        <div class="obj"></div>
-        <div class="obj"></div>
-        <div class="obj"></div>
+      <div className="loading">
+        <div className="obj"></div>
+        <div className="obj"></div>
+        <div className="obj"></div>
+        <div className="obj"></div>
+        <div className="obj"></div>
+        <div className="obj"></div>
+        <div className="obj"></div>
+        <div className="obj"></div>
       </div>
     );
   }
