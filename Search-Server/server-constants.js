216dd676be07b4abb4a3da908d9bf4af6cb097f9
MuhamedAbdalla <medokingdom7@outlook.com>
2020-12-23 17:37:09 +0200

    Init postManager

--- Search-Server/server-constants.js
+++ Search-Server/server-constants.js
@@ -1,17 +1,21 @@
-export const DatabaseUrl = "https://game-nav.herokuapp.com";
-export const USER_COLLECTION_ENTRY = "UserCollection";
-export const USER_ID_ENTRY = "ID";
-export const POST_COLLECTION_ENTRY = "PostCollection";
-export const DatabaseConstants = [
-  "GREATER_THAN_OR_EQUAL",
-  "START_AFTER_DOCUMENT",
-  "LESS_THAN_OR_EQUAL",
-  "GREATER_THAN",
-  "CONTAINS_ANY",
-  "LESS_THAN",
-  "CONTAINS",
-  "ORDER_BY",
-  "EQUALS",
-  "LIMIT",
-];
-  
+module.exports = {
+  DatabaseUrl: "https://game-nav.herokuapp.com",
+  //------------------------------User cons----------------------------------------------//
+  USER_COLLECTION_ENTRY: "UserCollection",
+  USER_ID_ENTRY: "ID",
+  //------------------------------Post cons----------------------------------------------//
+  POST_COLLECTION_ENTRY: "PostCollection",
+  //------------------------------Database Cons-----------------------------------------------//
+  DatabaseConstants: [
+    "GREATER_THAN_OR_EQUAL",
+    "START_AFTER_DOCUMENT",
+    "LESS_THAN_OR_EQUAL",
+    "GREATER_THAN",
+    "CONTAINS_ANY",
+    "LESS_THAN",
+    "CONTAINS",
+    "ORDER_BY",
+    "EQUALS",
+    "LIMIT",
+  ],
+};
