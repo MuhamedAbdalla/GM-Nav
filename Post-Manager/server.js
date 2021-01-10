4ab33ec03e79c18021e428c673077be6ee7fd9c9
MuhamedAbdalla <medokingdom7@outlook.com>
2021-01-10 16:49:02 +0200

    add posts

--- Post-Manager/server.js
+++ Post-Manager/server.js
@@ -9,11 +9,43 @@ const DB_URL = constants.DatabaseUrl;
 const app = express();
 
 app.post("/Add-Post", async (req, res) => {
-    
+  await fetch(DB_URL + '/insert', {
+    method: 'post',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({
+      path: [constants.POST_COLLECTION_ENTRY],
+      model: req.body.model,
+    }),
+  })
+  .then((_) => {
+    res.send('success');
+  })
+  .catch((error) => {
+    console.log('Error Login: ' + error);
+    res.send('fail');
+  });
 });
 
 app.post("/update-Post", async (req, res) => {
   // TODO: to be implemented, FUTURE UPDATES!!
+});
+
+app.post("/mark-Post", async (req, res) => {
+  await fetch(DB_URL + '/update', {
+    method: 'post',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({
+      path: [constants.POST_COLLECTION_ENTRY],
+      model: req.body.model,
+    }),
+  })
+  .then((_) => {
+    res.send('success');
+  })
+  .catch((error) => {
+    console.log('Error Login: ' + error);
+    res.send('fail');
+  });
 });
 
 app.listen(3030, () => {
